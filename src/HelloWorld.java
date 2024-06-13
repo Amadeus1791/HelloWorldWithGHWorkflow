31035094d76c4123417cabea95e04c19ce4464ae
Amadeus1791 <schedl.wolfgang@gmail.com>
2024-06-13 15:36:07 +0200

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        MyMath myMath = new MyMath();
+        int num = myMath.add(1, 3);
+        System.out.println("hello world");
+        System.out.println(num);
+    }
+}
